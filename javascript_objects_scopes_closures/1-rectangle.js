458b3f6f157899199032136c944987c3aad56ba3
Bruno-te <i.bruno@alustudent.com>
2025-01-29 10:09:10 +0200

    javascript_objects_scopes_closures

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) { // Add space between "constructor" and "("
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
